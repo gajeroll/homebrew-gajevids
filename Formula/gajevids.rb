d3a2fb5945408752f356dd4cdd498e59466a7d6b
cardseditor <toshiki.u314@gmail.com>
2025-07-05 23:17:34 +0900

    fix: Update homepage and URL to reflect correct repository name

--- Formula/gajevids.rb
+++ Formula/gajevids.rb
@@ -1,10 +1,10 @@
 class Gajevids < Formula
   desc "YAML-based FFmpeg video composition tool"
-  homepage "https://github.com/gajeroll/gajevids"
-  url "https://github.com/gajeroll/gajevids/archive/refs/tags/v0.1.0.tar.gz"
+  homepage "https://github.com/gajeroll/gajevids-homebrew"
+  url "https://github.com/gajeroll/gajevids-homebrew/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
-  head "https://github.com/gajeroll/gajevids.git", branch: "main"
+  head "https://github.com/gajeroll/gajevids-homebrew.git", branch: "main"
 
   depends_on "go" => :build
   depends_on "ffmpeg"
