5963d517db8183a20db00817b67d3e4cafe4bb27
chnotchy <kokiuaxdx@gmail.com>
2025-07-06 01:50:10 +0900

    feat: Add Gajevids formula for Homebrew installation

---
+++ Formula/gajevids.rb
@@ -0,0 +1,21 @@
+class Gajevids < Formula
+  VERSION = "v0.2.1"
+  desc     "YAML-based FFmpeg video composition tool"
+  homepage "https://github.com/gajeroll/homebrew-gajevids"
+  url      "https://github.com/gajeroll/homebrew-gajevids/archive/refs/tags/#{VERSION}.tar.gz"
+  sha256   "TO_BE_REPLACED"
+  license  "MIT"
+  head     "https://github.com/gajeroll/homebrew-gajevids.git", branch: "main"
+
+  depends_on "ffmpeg"
+  depends_on "yq"
+
+  def install
+    bin.install "dist/gajevids"
+    bin.install "dist/gajevids-go"
+  end
+
+  test do
+    assert_match "gajevids", shell_output("#{bin}/gajevids --version")
+  end
+end
