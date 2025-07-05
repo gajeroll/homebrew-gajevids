2ed50c20ab09d444636a82aca6f667d87cd37f1b
chnotchy <kokiuaxdx@gmail.com>
2025-07-06 02:02:01 +0900

    gajevids v0.2.5 (dist layout)

--- Formula/gajevids.rb
+++ Formula/gajevids.rb
@@ -1,5 +1,5 @@
 class Gajevids < Formula
-  VERSION = "v0.2.4"
+  VERSION = "v0.2.5"
   desc     "YAML-based FFmpeg video composition tool"
   homepage "https://github.com/gajeroll/homebrew-gajevids"
   url      "https://github.com/gajeroll/homebrew-gajevids/archive/refs/tags/#{VERSION}.tar.gz"
