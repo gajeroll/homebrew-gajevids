5377fe9447fa57571e7fde72bc45e3b9864d79c4
chnotchy <kokiuaxdx@gmail.com>
2025-07-09 15:40:37 +0900

    chore: release gajevids v0.3.48

--- Formula/gajevids.rb
+++ Formula/gajevids.rb
@@ -1,10 +1,10 @@
 class Gajevids < Formula
   desc "YAML-based FFmpeg video composition tool"
   homepage "https://github.com/gajeroll/homebrew-gajevids"
-  version "0.3.47"
-  sha256 "b22dc924a903754588e08ebb544d36857fca99b4318ab56f9e86a2a6ad976bf5"
+  version "0.3.48"
+  sha256 "6d4a0264a8e1c310d0fb8ab32dd7e2d37b70687c62a8af2f50faa9a8dead31b0"
 
-  url "https://github.com/gajeroll/homebrew-gajevids/releases/download/v0.3.47/gajevids_0.3.47_darwin_arm64.tar.gz"
+  url "https://github.com/gajeroll/homebrew-gajevids/releases/download/v0.3.48/gajevids_0.3.48_darwin_arm64.tar.gz"
 
   def install
     bin.install "gajevids"
